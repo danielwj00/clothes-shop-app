2b2531e465618d16ea1339739b36d97f66a64e08
Daniel W <danielwj00@gmail.com>
2021-04-29 03:09:30 +0800

    Directory Reducer
    Moved directory and shop data to its own reducer, create its selectors, and update component with new redux flow for directory/shop

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,23 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import localStorage from "redux-persist/es/storage";
+
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 import userReducer from "./user/user.reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage: localStorage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
