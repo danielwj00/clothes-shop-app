25bcb21d84ceb3224bab674cbbae87b8ad323d2e
Daniel W <danielwj00@gmail.com>
2021-04-29 22:54:08 +0800

    Update Stripe integration and favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51IlXRdIHgcNK7j0vSaCmH0BplFTLECmcXD4Mn7PKgcZauKO5EgjZaWcL1MvM9bPxrjVN96nJuuFJLAXsX0IFecjj00OTH1LyWa";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="D Clothing"
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
