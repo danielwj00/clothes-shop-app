6f1209c4d78976c662fdb05343a44683d6f72392
Daniel W <danielwj00@gmail.com>
2021-05-05 03:53:42 +0800

    Redux Thunk and Redux Saga Implement
    Module 20 part 5

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,40 @@
+import {
+  convertCollectionSnapshotToMap,
+  firestore,
+} from "../../firebase/firebase.utils";
 import ShopActionTypes from "./shop.types";
 
-export const updateCollections = (collectionsMap) => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionsMap,
 });
+
+export const fetchCollectionsFailure = (errorMessage) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage,
+});
+
+export const fetchCollectionsStartAsync = () => {
+  return (dispatch) => {
+    const collectionRef = firestore.collection("collections");
+    dispatch(fetchCollectionsStart());
+
+    // fetch(
+    //   "https://firestore.googleapis.com/v1/projects/clothes-shop-app/databases/(default)/documents/collections"
+    // )
+    //   .then((response) => response.json())
+    //   .then((collections) => console.log(collections));
+
+    collectionRef
+      .get()
+      .then((snapshot) => {
+        const collectionsMap = convertCollectionSnapshotToMap(snapshot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));
+      })
+      .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
+  };
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,22 @@
 import { applyMiddleware, createStore } from "redux";
 import logger from "redux-logger";
-import { persistStore } from 'redux-persist';
+import { persistStore } from "redux-persist";
+// import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [];
+const sagaMiddleware = createSagaMiddleware();
 
-if(process.env.NODE_ENV === 'development'){
-    middlewares.push(logger);
+const middlewares = [sagaMiddleware];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
 }
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+// sagaMiddleware.run()
 
 const persistor = persistStore(store);
 
