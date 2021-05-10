9084d8ffddc358e4b7ee0360f6569267c714a27a
Daniel W <danielwj00@gmail.com>
2021-05-11 01:38:36 +0800

    Separate app to client folder, added express server handle stripe
    react app moved to client folder.
    Added Express server to handle backend stripe

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,8 +8,23 @@ const StripeCheckoutButton = ({ price }) => {
     "pk_test_51IlXRdIHgcNK7j0vSaCmH0BplFTLECmcXD4Mn7PKgcZauKO5EgjZaWcL1MvM9bPxrjVN96nJuuFJLAXsX0IFecjj00OTH1LyWa";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please sure you use the provided credit card."
+        );
+      });
   };
 
   return (
